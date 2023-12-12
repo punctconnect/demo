46b9346dc70fef5f1e0b4cc26ccfa95c0a99bf3c
Bogdan Piele <bogdanpielemail@gmail.com>
2023-12-12 23:24:35 +0200

    Fixed form submit and error validation

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import { TextField, Button, Box, Typography, Container } from '@material-ui/core';
 import { useRouter } from 'next/router';
@@ -8,59 +8,56 @@ export default function Form() {
     const router = useRouter();
     const [state, handleSubmit] = useForm("mvojengb");
 
-    const handleFormSubmit = async (event) => {
+    const handleFormSubmit = (event) => {
         event.preventDefault();
-        const submissionResult = await handleSubmit(event);
-
-        console.log(submissionResult);
-
-        if (submissionResult === undefined || submissionResult === null) {
-            alert("Failed validation. Please check your inputs and try again.");
-            return;
-        }
-
-        if (submissionResult.status !== 200) {
-            alert("Failed to submit form. Please check your inputs and try again.");
-            return;
-        }
-
-        if (submissionResult.succeeded === false) {
-            alert("Failed to submit form. Please check your inputs and try again.");
-            return;
-        }
-
-        alert("Thank you for your submission!");
-        router.push("/");
+        handleSubmit(event);
     };
 
+    useEffect(() => {
+        if (state.succeeded) {
+            alert("Thanks for subscribing!");
+            router.push('/');
+        }
+    }, [router, state.succeeded]);
+
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center"
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
             style={{
-                width: '100%', margin: 'auto', maxWidth: '600px', padding: '1rem', marginTop: '2rem',
-                alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column'
-            }}>
+                width: '100%',
+                margin: 'auto',
+                maxWidth: '600px',
+                padding: '1rem',
+                marginTop: '2rem',
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+        >
             <Typography variant="h3" component="h1" gutterBottom>
                 Subscribe to our newsletter
             </Typography>
 
-            <form onSubmit={handleFormSubmit} style={{
-
-            }}>
-
+            <form onSubmit={handleFormSubmit}>
                 <TextField
                     id="email"
                     type="email"
                     name="email"
                     label="Email Address"
                     fullWidth
+                    inputProps={{ inputMode: 'email' }}
                     margin="normal"
+                    required
                 />
                 <ValidationError
                     prefix="Email"
                     field="email"
                     errors={state.errors}
                 />
-
 
                 <TextField
                     id="phone"
@@ -69,6 +66,8 @@ export default function Form() {
                     label="Phone Number"
                     fullWidth
                     margin="normal"
+                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                    required
                 />
                 <ValidationError
                     prefix="Phone"
@@ -105,9 +104,5 @@ export default function Form() {
 }
 
 Form.getLayout = function getLayout(page) {
-    return (
-        <Layout>
-            {page}
-        </Layout>
-    )
-}
+    return <Layout>{page}</Layout>;
+};
