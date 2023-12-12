75d0d19ba69083076f0e5224dae177ea03fe2f16
Bogdan Piele <bogdanpielemail@gmail.com>
2023-12-12 21:38:41 +0200

    removed leftover files, improved file structure

--- layouts/layout.jsx
+++ layouts/layout.jsx
@@ -1,23 +1,11 @@
-import { AppBar, Toolbar } from "@mui/material";
-
-
+import React from "react";
+import Header from "../components/Header";
 
 export default function Layout({ children }) {
     return (
         <>
-
-            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <h1>Lab 4</h1>
-                    </Toolbar>
-                </AppBar>
-                <div style={{ flex: '1' }}>
-                    {children}
-                </div>
-                <footer style={{ background: 'black', color: 'white', textAlign: 'center' }}>Footer</footer>
-            </div>
-
+            <Header></Header>
+            <main>{children}</main>
         </>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import Form from "../components/Form";
-
+import { Container, Typography, Button, Grid, Card, CardContent } from '@material-ui/core';
+import Headerless from '../layouts/headerless';
 
 export default function HomePage() {
-
-
-
   return (
-      <div style={{padding: "20px"}}>
-        <Form/>
-      
-      </div>
+    <Container>
+      <Grid container spacing={3} justify="center" alignItems="center" style={{ minHeight: '80vh' }}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h2" component="h1" gutterBottom>
+            Welcome to PunctConnect
+          </Typography>
+          <Typography variant="h5" color="textSecondary" paragraph>
+            Your personalized coupon and discount hub. Start saving today!
+          </Typography>
+          <Button variant="contained" color="primary" size="large">
+            Explore Deals
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6" component="h2" gutterBottom>
+                Featured Coupons
+              </Typography>
+              {/* Add your featured coupons or deals here */}
+              <Typography color="textSecondary" paragraph>
+                Check out these exclusive offers from our partners.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
+}
+
+HomePage.getLayout = function getLayout(page) {
+  return (
+    <Headerless>
+      {page}
+    </Headerless>
+  )
 }
