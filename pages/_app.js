1533ac8fbbff61b5cfafc2dc51e53f5f26ade4c7
Preda Mihai <predamihai14@hotmail.com>
2023-12-14 01:10:34 +0200

    feat: add posthog and hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,69 @@
 // pages/app.js
 
-import React from 'react';
-import Head from 'next/head';
-import { Container, Typography, Button, Grid, Card, CardContent } from '@material-ui/core';
-import Layout from '../layouts/layout';
-import '../styles/globals.css';
+import Hotjar from "@hotjar/browser";
+import { Container, Typography } from "@material-ui/core";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import React, { useEffect } from "react";
+import "../styles/globals.css";
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+  posthog.init("phc_j9yScYcBMQB4qIgJTnWWuBnDPYfp0CcqUNK4yr2yA9g", {
+    api_host: "https://eu.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 const AppPage = ({ Component, pageProps }) => {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(
-    <Container>
-      <Head>
-        <title>PunctConnect - Your Coupon and Discount Hub</title>
-        <meta name="description" content="Find the best deals and discounts with PunctConnect" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const router = useRouter();
 
-      <Component {...pageProps} />
+  useEffect(() => {
+    // Track page views
+    Hotjar.init(3793789, 6);
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
 
-      <footer>
-        <Typography variant="body2" color="textSecondary" align="center">
-          &copy; {new Date().getFullYear()} PunctConnect. All rights reserved.
-        </Typography>
-      </footer>
-    </Container>
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <PostHogProvider client={posthog}>
+      <>
+        {" "}
+        {getLayout(
+          <Container>
+            <Head>
+              <title>PunctConnect - Your Coupon and Discount Hub</title>
+              <meta
+                name="description"
+                content="Find the best deals and discounts with PunctConnect"
+              />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Component {...pageProps} />
+
+            <footer>
+              <Typography variant="body2" color="textSecondary" align="center">
+                &copy; {new Date().getFullYear()} PunctConnect. All rights
+                reserved.
+              </Typography>
+            </footer>
+          </Container>
+        )}
+      </>
+    </PostHogProvider>
   );
 };
 
