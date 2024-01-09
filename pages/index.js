32d177db14c1165d07c2179fad55edf29e73326d
Bogdan Piele <bogdanpielemail@gmail.com>
2024-01-09 22:41:07 +0200

    build failure fix #2

--- pages/index.js
+++ pages/index.js
@@ -83,6 +83,7 @@ export default function HomePage() {
               <Button variant="contained" onClick={handleClick}>
                 Subscribe
               </Button>
+              
             </CardContent>
           </Card>
 
